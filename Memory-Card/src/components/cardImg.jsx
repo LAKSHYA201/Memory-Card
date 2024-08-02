bab69803aff8f3a9e0a277ec8cf4d1b74fdd10ad
Lakshya Tyagi <lakshyatyagi2005@gmail.com>
2024-08-02 21:26:51 +0530

    add:imgFetcher fn in cardIMg.jsx using fetchAPI

---
+++ Memory-Card/src/components/cardImg.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+export default function CardImg({ cardNo }) {
+  const [imgSrc, setImgSrc] = useState("");
+
+  async function imgFetcher(cardNo) {
+    const apiAddress = "https://akabab.github.io/starwars-api/api/id/";
+    let completeApiAddress = apiAddress + cardNo + ".json";
+    await fetch(completeApiAddress, { mode: "cors" })
+      .then((response) => response.json())
+      .then((data) => setImgSrc(data.image));
+  }
+  imgFetcher(cardNo);
+
+  return <img src={imgSrc} height="100%" width="100%" />;
+}
--- Memory-Card/src/components/pageLayout/mainApplication.jsx
+++ Memory-Card/src/components/pageLayout/mainApplication.jsx
@@ -1,17 +1,39 @@
 import "../../styles/mainApplication.css";
+import CardImg from "../cardImg";
+
 export default function MainApplication() {
   return (
     <div className="mainApplication">
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
+      <div className="card">
+        <CardImg cardNo={1} />
+      </div>
+      <div className="card">
+        <CardImg cardNo={2} />
+      </div>
+      <div className="card">
+        <CardImg cardNo={3} />
+      </div>
+      <div className="card">
+        <CardImg cardNo={4} />
+      </div>
+      <div className="card">
+        <CardImg cardNo={5} />
+      </div>
+      <div className="card">
+        <CardImg cardNo={6} />
+      </div>
+      <div className="card">
+        <CardImg cardNo={7} />
+      </div>
+      <div className="card">
+        <CardImg cardNo={8} />
+      </div>
+      <div className="card">
+        <CardImg cardNo={9} />
+      </div>
+      <div className="card">
+        <CardImg cardNo={10} />
+      </div>
     </div>
   );
 }
