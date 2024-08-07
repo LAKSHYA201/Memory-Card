6cd731b94b2901a5822cdfedea160b6b5a4f1d77
Lakshya Tyagi <lakshyatyagi2005@gmail.com>
2024-08-07 11:32:15 +0530

    add:src paths

--- Memory-Card/src/helper/cardsIndex.js
+++ Memory-Card/src/helper/cardsIndex.js
@@ -2,43 +2,43 @@ import { v4 as uuidv4 } from "uuid";
 
 const starCards = [
   {
-    src: "./src/assets/1.webp",
+    src: "./Memory-Card/src/assets/1.webp",
     id: uuidv4(),
   },
   {
-    src: "./src/assets/2.webp",
+    src: "./Memory-Card/src/assets/2.webp",
     id: uuidv4(),
   },
   {
-    src: "./src/assets/3.webp",
+    src: "./Memory-Card/src/assets/3.webp",
     id: uuidv4(),
   },
   {
-    src: "./src/assets/4.webp",
+    src: "./Memory-Card/src/assets/4.webp",
     id: uuidv4(),
   },
   {
-    src: "./src/assets/5.webp",
+    src: "./Memory-Card/src/assets/5.webp",
     id: uuidv4(),
   },
   {
-    src: "./src/assets/6.webp",
+    src: "./Memory-Card/src/assets/6.webp",
     id: uuidv4(),
   },
   {
-    src: "./src/assets/7.webp",
+    src: "./Memory-Card/src/assets/7.webp",
     id: uuidv4(),
   },
   {
-    src: "./src/assets/8.webp",
+    src: "./Memory-Card/src/assets/8.webp",
     id: uuidv4(),
   },
   {
-    src: "./src/assets/9.webp",
+    src: "./Memory-Card/src/assets/9.webp",
     id: uuidv4(),
   },
   {
-    src: "./src/assets/10.webp",
+    src: "./Memory-Card/src/assets/10.webp",
     id: uuidv4(),
   },
 ];
