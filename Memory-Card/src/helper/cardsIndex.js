1b772cde257ddf0b84b436943d1f14fc8eaad0af
Lakshya Tyagi <lakshyatyagi2005@gmail.com>
2024-08-07 11:49:29 +0530

    add:api based src paths for img

--- Memory-Card/src/helper/cardsIndex.js
+++ Memory-Card/src/helper/cardsIndex.js
@@ -2,43 +2,43 @@ import { v4 as uuidv4 } from "uuid";
 
 const starCards = [
   {
-    src: "./Memory-Card/src/assets/1.webp",
+    src: "https://vignette.wikia.nocookie.net/starwars/images/8/8f/Lando_ROTJ.png",
     id: uuidv4(),
   },
   {
-    src: "./Memory-Card/src/assets/2.webp",
+    src: "https://vignette.wikia.nocookie.net/starwars/images/7/79/Boba_Fett_HS_Fathead.png",
     id: uuidv4(),
   },
   {
-    src: "./Memory-Card/src/assets/3.webp",
+    src: "https://vignette.wikia.nocookie.net/starwars/images/c/c6/Greedo.jpg",
     id: uuidv4(),
   },
   {
-    src: "./Memory-Card/src/assets/4.webp",
+    src: "https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg",
     id: uuidv4(),
   },
   {
-    src: "./Memory-Card/src/assets/5.webp",
+    src: "https://vignette.wikia.nocookie.net/starwars/images/2/29/Admiral_Ackbar_RH.png",
     id: uuidv4(),
   },
   {
-    src: "./Memory-Card/src/assets/6.webp",
+    src: "https://vignette.wikia.nocookie.net/starwars/images/7/79/Poe_Dameron_TLJ.png",
     id: uuidv4(),
   },
   {
-    src: "./Memory-Card/src/assets/7.webp",
+    src: "https://vignette.wikia.nocookie.net/starwars/images/f/fd/Nute_Gunray_SWE.png",
     id: uuidv4(),
   },
   {
-    src: "./Memory-Card/src/assets/8.webp",
+    src: "https://vignette.wikia.nocookie.net/starwars/images/b/b8/Dooku_Headshot.jpg",
     id: uuidv4(),
   },
   {
-    src: "./Memory-Card/src/assets/9.webp",
+    src: "https://vignette.wikia.nocookie.net/starwars/images/4/4e/ObiWanHS-SWE.jpg",
     id: uuidv4(),
   },
   {
-    src: "./Memory-Card/src/assets/10.webp",
+    src: "https://vignette.wikia.nocookie.net/fr.starwars/images/3/32/Dark_Vador.jpg",
     id: uuidv4(),
   },
 ];
